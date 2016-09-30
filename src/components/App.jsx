88fd7c32326dde94f7c4f00512c34dc9bd547b34
Christopher Tham <chriswt91@gmail.com>
2016-09-30 16:25:14 -0700

    Implement video player and stateful app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer />
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={exampleVideoData} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+  }
 
-console.log(exampleVideoData);
+  onVideoClick() {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={exampleVideoData[0]} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={exampleVideoData} />
+        </div>
+      </div>
+    );
+  }
+};
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.App = App();
+window.App = App;

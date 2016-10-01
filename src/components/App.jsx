7add731fd670db33ca734e7ebf09f5b1a9c667ee
chrswt <chriswt91@gmail.com>
2016-09-30 20:47:27 -0700

    Implement searchYouTube using componentDidMount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,43 @@
 class App extends React.Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
+    // debugger;
+    console.log(props);
+    // console.log(props.searchYouTube({query: 'wedidit', max: 10, key: window.YOUTUBE_API_KEY}, function() { console.log('success'); } ));
     this.state = {
-      video: exampleVideoData[0]
+      video: exampleVideoData[0],
+      results: exampleVideoData
     };
   }
 
   onVideoClick(i, data) {
-    // debugger;
-    // console.log('clicked', data, a, b);
     this.setState({video: data[i]});
   }
+
+  componentDidMount() {
+    this.props.searchYouTube({query: 'wedidit', max: 10, key: window.YOUTUBE_API_KEY},
+      (dataItems) => {
+        this.setState({
+          video: dataItems[0],
+          results: dataItems
+        });
+      });
+    // console.log(results);
+    // this.setState({
+  }
+
+
 
   render() {
     return (
       <div>
         <Nav />
+        
         <div className="col-md-7">
           <VideoPlayer video={this.state.video} />
         </div>
         <div className="col-md-5">
-          <VideoList videos={exampleVideoData} onVideoClick={this.onVideoClick.bind(this)}/>
+          <VideoList videos={this.state.results} onVideoClick={this.onVideoClick.bind(this)}/>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 // TODO: Render the `App` component to the DOM
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App searchYouTube={searchYouTube} />, document.getElementById('app'));
+
+// options={''} callback={''}
